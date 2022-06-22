09019dcf736aec63ba384046041967af379922f0
StevenCloud <stevencloud9@gmail.com>
2022-06-22 16:49:00 -0500

    moved topl.py, adding PAT locally

--- topl_temp/topl.py
+++
@@ -1,7 +0,0 @@
-from BramblPy import brambl
-from BramblPy.brambl.credentials import credential_manager
-
-keyMan = credential_manager(password = "PASSWORD", networkPrefix = "valhalla")
-
-dirPath = "./keyfiles"
-keyMan.exportToFile(dirPath)
